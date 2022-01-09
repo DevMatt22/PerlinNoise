a349cc6f374e59526ac745852cc3f195714ccbaf
Matthieu MARTIN <martin.matt820@gmail.com>
2022-01-08 22:50:31 -0500

    Add Mesh

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class MapGenerator : MonoBehaviour
 {
 
-    public enum DrawMode { NoiseMap, ColourMap}
+    public enum DrawMode { NoiseMap, ColourMap, Mesh}
     public DrawMode drawMode;
 
     public int mapWidth;
@@ -55,6 +55,11 @@ public class MapGenerator : MonoBehaviour
         {
             display.DrawTexture(TextureGenerator.TextureFromColourMap(colourMap, mapWidth, mapHeight));
         }  
+
+        else if(drawMode == DrawMode.Mesh)
+        {
+            display.DrawMesh(MeshGenerator.GenerateTerrainMesh(noiseMap), TextureGenerator.TextureFromColourMap(colourMap, mapWidth, mapHeight));
+        }
     }
 
     private void OnValidate()
